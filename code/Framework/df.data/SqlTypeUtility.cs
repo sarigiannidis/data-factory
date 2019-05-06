bc90384d376ba37202e7d11e4bd82ba71ae3bd36
Michalis Sarigiannidis <3284819+sarigiannidis@users.noreply.github.com>
2019-05-06 17:12:44 +0200

    complexity

--- code/Framework/df.data/SqlTypeUtility.cs
+++ code/Framework/df.data/SqlTypeUtility.cs
@@ -8,11 +8,49 @@
 namespace Df.Data
 {
     using System;
+    using System.Collections.Generic;
     using System.Text;
     using static Constants;
 
     public static class SqlTypeUtility
     {
+        private static readonly Dictionary<string, Type> _Types = new Dictionary<string, Type>
+        {
+            { SQL_TYPE_DATE, typeof(DateTime) },
+            { SQL_TYPE_DATETIME, typeof(DateTime) },
+            { SQL_TYPE_DATETIME2, typeof(DateTime) },
+            { SQL_TYPE_SMALLDATETIME, typeof(DateTime) },
+            { SQL_TYPE_DATETIMEOFFSET, typeof(DateTimeOffset) },
+            { SQL_TYPE_TIME, typeof(TimeSpan) },
+            { SQL_TYPE_CHAR, typeof(char) },
+            { SQL_TYPE_NCHAR, typeof(char) },
+            { SQL_TYPE_VARCHAR, typeof(char) },
+            { SQL_TYPE_NVARCHAR, typeof(char) },
+            { SQL_TYPE_SYSNAME, typeof(string) },
+            { SQL_TYPE_NTEXT, typeof(string) },
+            { SQL_TYPE_TEXT, typeof(string) },
+            { SQL_TYPE_XML, typeof(string) },
+            { SQL_TYPE_DECIMAL, typeof(decimal) },
+            { SQL_TYPE_NUMERIC, typeof(decimal) },
+            { SQL_TYPE_SMALLMONEY, typeof(decimal) },
+            { SQL_TYPE_MONEY, typeof(decimal) },
+            { SQL_TYPE_BIT, typeof(bool) },
+            { SQL_TYPE_TINYINT, typeof(byte) },
+            { SQL_TYPE_SMALLINT, typeof(short) },
+            { SQL_TYPE_INT, typeof(int) },
+            { SQL_TYPE_BIGINT, typeof(long) },
+            { SQL_TYPE_REAL, typeof(float) },
+            { SQL_TYPE_FLOAT, typeof(double) },
+            { SQL_TYPE_UNIQUEIDENTIFIER, typeof(Guid) },
+            { SQL_TYPE_BINARY, typeof(byte[]) },
+            { SQL_TYPE_VARBINARY, typeof(byte[]) },
+            { SQL_TYPE_IMAGE, typeof(byte[]) },
+            { SQL_TYPE_TIMESTAMP, typeof(byte[]) },
+            { SQL_TYPE_HIERARCHYID, typeof(Microsoft.SqlServer.Types.SqlHierarchyId) },
+            { SQL_TYPE_GEOMETRY, typeof(Microsoft.SqlServer.Types.SqlGeometry) },
+            { SQL_TYPE_GEOGRAPHY, typeof(Microsoft.SqlServer.Types.SqlGeography) },
+        };
+
         public static Type GetDataType(string dataTypeName, short columnSize, bool allowDbNull)
         {
             var dataType = GetDataType(dataTypeName, columnSize);
@@ -29,79 +67,8 @@ namespace Df.Data
 
         public static Type GetDataType(string dataTypeName, short columnSize)
         {
-            switch (dataTypeName)
-            {
-                case SQL_TYPE_DATE:
-                case SQL_TYPE_DATETIME:
-                case SQL_TYPE_DATETIME2:
-                case SQL_TYPE_SMALLDATETIME:
-                    return typeof(DateTime);
-
-                case SQL_TYPE_DATETIMEOFFSET:
-                    return typeof(DateTimeOffset);
-
-                case SQL_TYPE_TIME:
-                    return typeof(TimeSpan);
-
-                case SQL_TYPE_CHAR:
-                case SQL_TYPE_NCHAR:
-                case SQL_TYPE_VARCHAR:
-                case SQL_TYPE_NVARCHAR:
-                    return (columnSize == 1) ? typeof(char) : typeof(string);
-
-                case SQL_TYPE_SYSNAME:
-                case SQL_TYPE_NTEXT:
-                case SQL_TYPE_TEXT:
-                case SQL_TYPE_XML:
-                    return typeof(string);
-
-                case SQL_TYPE_DECIMAL:
-                case SQL_TYPE_NUMERIC:
-                case SQL_TYPE_SMALLMONEY:
-                case SQL_TYPE_MONEY:
-                    return typeof(decimal);
-
-                case SQL_TYPE_BIT:
-                    return typeof(bool);
-
-                case SQL_TYPE_TINYINT:
-                    return typeof(byte);
-
-                case SQL_TYPE_SMALLINT:
-                    return typeof(short);
-
-                case SQL_TYPE_INT:
-                    return typeof(int);
-
-                case SQL_TYPE_BIGINT:
-                    return typeof(long);
-
-                case SQL_TYPE_REAL:
-                    return typeof(float);
-
-                case SQL_TYPE_FLOAT:
-                    return typeof(double);
-
-                case SQL_TYPE_UNIQUEIDENTIFIER:
-                    return typeof(Guid);
-
-                case SQL_TYPE_BINARY:
-                case SQL_TYPE_VARBINARY:
-                case SQL_TYPE_IMAGE:
-                case SQL_TYPE_TIMESTAMP:
-                    return typeof(byte[]);
-
-                case SQL_TYPE_HIERARCHYID:
-                    return typeof(Microsoft.SqlServer.Types.SqlHierarchyId);
-
-                case SQL_TYPE_GEOMETRY:
-                    return typeof(Microsoft.SqlServer.Types.SqlGeometry);
-
-                case SQL_TYPE_GEOGRAPHY:
-                    return typeof(Microsoft.SqlServer.Types.SqlGeography);
-                default:
-                    return typeof(object);
-            }
+            var baseType = GetBaseType(dataTypeName);
+            return columnSize > 1 && baseType == typeof(char) ? typeof(string) : baseType;
         }
 
         public static string GetSqlColumnDefinition(string name, string userType, bool nullable, short maxLength, short precision, short scale, object seedValue, object incrementValue)
@@ -126,6 +93,9 @@ namespace Df.Data
             return sb.ToString();
         }
 
+        private static Type GetBaseType(string dataTypeName) =>
+                    _Types.TryGetValue(dataTypeName, out var value) ? value : typeof(object);
+
         private static string GetSqlType(string userType, short maxLength, short precision, short scale)
         {
             var sb = new StringBuilder(userType.ToUpperInvariant());
@@ -133,36 +103,36 @@ namespace Df.Data
             {
                 case SQL_TYPE_NCHAR:
                 case SQL_TYPE_NVARCHAR:
+                {
+                    maxLength /= 2;
+                    if (maxLength > 0)
                     {
-                        maxLength /= 2;
-                        if (maxLength > 0)
-                        {
                         _ = sb.AppendFormatInvariant("({0})", maxLength);
-                        }
-                        else if (maxLength == -1)
-                        {
-                        _ = sb.Append("(MAX)");
-                        }
-
-                        break;
                     }
+                    else if (maxLength == -1)
+                    {
+                        _ = sb.Append("(MAX)");
+                    }
+
+                    break;
+                }
 
                 case SQL_TYPE_CHAR:
                 case SQL_TYPE_VARCHAR:
                 case SQL_TYPE_BINARY:
                 case SQL_TYPE_VARBINARY:
+                {
+                    if (maxLength > 0)
                     {
-                        if (maxLength > 0)
-                        {
                         _ = sb.AppendFormatInvariant("({0})", maxLength);
-                        }
-                        else if (maxLength == -1)
-                        {
-                        _ = sb.Append("(MAX)");
-                        }
-
-                        break;
                     }
+                    else if (maxLength == -1)
+                    {
+                        _ = sb.Append("(MAX)");
+                    }
+
+                    break;
+                }
 
                 case SQL_TYPE_FLOAT:
                     _ = sb.AppendFormatInvariant("({0})", precision);
