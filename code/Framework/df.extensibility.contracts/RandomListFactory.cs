c8d1eee006a1ace3e661274a8f432792a9dec4f0
Michalis Sarigiannidis <3284819+sarigiannidis@users.noreply.github.com>
2019-05-07 12:10:49 +0200

    .

--- code/Framework/df.extensibility.contracts/RandomListFactory.cs
+++ code/Framework/df.extensibility.contracts/RandomListFactory.cs
@@ -7,13 +7,33 @@
 
 namespace Df.Extensibility
 {
+    using Df.Stochastic;
     using System;
+    using System.Linq;
 
     public abstract class RandomListFactory<TValue>
-                : RandomFactory<TValue, IListFactoryConfiguration<TValue>>
-            where TValue : struct, IComparable, IFormattable, IComparable<TValue>, IEquatable<TValue>
+        : RandomFactory<TValue, IListFactoryConfiguration<TValue>>
+        where TValue : struct, IComparable, IFormattable, IComparable<TValue>, IEquatable<TValue>
     {
-        public override TValue CreateValue() =>
+        protected float SumOfWeights { get; private set; }
+
+        protected RandomListFactory() =>
+            ConfigurationChanged += (sender, e) => SumOfWeights = Configuration.WeightedValues.Sum(w => w.Weight);
+
+        public override TValue CreateValue()
+        {
+            var value = Random.NextDouble() * SumOfWeights;
+            foreach (var weightedValue in Configuration.WeightedValues)
+            {
+                if (value < weightedValue.Weight)
+                {
+                    return weightedValue.Value;
+                }
+
+                value -= weightedValue.Weight;
+            }
+
             throw null;
+        }
     }
 }
