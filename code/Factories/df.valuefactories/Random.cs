f1e5f411df1eda624a16400522e750df9b247221
Michalis Sarigiannidis <3284819+sarigiannidis@users.noreply.github.com>
2019-05-14 13:05:31 +0200

    generated code

--- code/Factories/df.valuefactories/Random.cs
+++ code/Factories/df.valuefactories/Random.cs
@@ -11,8 +11,10 @@ namespace Df.ValueFactories
 {
     using Df.Extensibility;
     using System;
+    using System.CodeDom.Compiler;
     using static Constants;
 
+    [GeneratedCode("df", "")]
     [ValueFactory("byte-random", "Generates random byte values", typeof(byte), typeof(RandomByteFactory))]
     public sealed partial class RandomByteFactory
         : RandomScalarFactory<byte>,
@@ -22,6 +24,7 @@ namespace Df.ValueFactories
             new RangeFactoryConfiguration<byte>(BYTE_MIN, BYTE_MAX);
     }
 
+    [GeneratedCode("df", "")]
     [ValueFactory("datetime-random", "Generates random DateTime values", typeof(DateTime), typeof(RandomDateTimeFactory))]
     public sealed partial class RandomDateTimeFactory
         : RandomScalarFactory<DateTime>,
@@ -31,6 +34,7 @@ namespace Df.ValueFactories
             new RangeFactoryConfiguration<DateTime>(DATETIME_MIN, DATETIME_MAX);
     }
 
+    [GeneratedCode("df", "")]
     [ValueFactory("datetimeoffset-random", "Generates random DateTimeOffset values", typeof(DateTimeOffset), typeof(RandomDateTimeOffsetFactory))]
     public sealed partial class RandomDateTimeOffsetFactory
         : RandomScalarFactory<DateTimeOffset>,
@@ -40,6 +44,7 @@ namespace Df.ValueFactories
             new RangeFactoryConfiguration<DateTimeOffset>(DATETIMEOFFSET_MIN, DATETIMEOFFSET_MAX);
     }
 
+    [GeneratedCode("df", "")]
     [ValueFactory("decimal-random", "Generates random decimal values", typeof(decimal), typeof(RandomDecimalFactory))]
     public sealed partial class RandomDecimalFactory
         : RandomScalarFactory<decimal>,
@@ -49,6 +54,7 @@ namespace Df.ValueFactories
             new RangeFactoryConfiguration<decimal>(DECIMAL_MIN, DECIMAL_MAX);
     }
 
+    [GeneratedCode("df", "")]
     [ValueFactory("double-random", "Generates random double values", typeof(double), typeof(RandomDoubleFactory))]
     public sealed partial class RandomDoubleFactory
         : RandomScalarFactory<double>,
@@ -58,6 +64,7 @@ namespace Df.ValueFactories
             new RangeFactoryConfiguration<double>(DOUBLE_MIN, DOUBLE_MAX);
     }
 
+    [GeneratedCode("df", "")]
     [ValueFactory("float-random", "Generates random float values", typeof(float), typeof(RandomFloatFactory))]
     public sealed partial class RandomFloatFactory
         : RandomScalarFactory<float>,
@@ -67,6 +74,7 @@ namespace Df.ValueFactories
             new RangeFactoryConfiguration<float>(FLOAT_MIN, FLOAT_MAX);
     }
 
+    [GeneratedCode("df", "")]
     [ValueFactory("int-random", "Generates random int values", typeof(int), typeof(RandomIntFactory))]
     public sealed partial class RandomIntFactory
         : RandomScalarFactory<int>,
@@ -76,6 +84,7 @@ namespace Df.ValueFactories
             new RangeFactoryConfiguration<int>(INT_MIN, INT_MAX);
     }
 
+    [GeneratedCode("df", "")]
     [ValueFactory("long-random", "Generates random long values", typeof(long), typeof(RandomLongFactory))]
     public sealed partial class RandomLongFactory
         : RandomScalarFactory<long>,
@@ -85,6 +94,7 @@ namespace Df.ValueFactories
             new RangeFactoryConfiguration<long>(LONG_MIN, LONG_MAX);
     }
 
+    [GeneratedCode("df", "")]
     [ValueFactory("short-random", "Generates random short values", typeof(short), typeof(RandomShortFactory))]
     public sealed partial class RandomShortFactory
         : RandomScalarFactory<short>,
@@ -94,6 +104,7 @@ namespace Df.ValueFactories
             new RangeFactoryConfiguration<short>(SHORT_MIN, SHORT_MAX);
     }
 
+    [GeneratedCode("df", "")]
     [ValueFactory("timespan-random", "Generates random TimeSpan values", typeof(TimeSpan), typeof(RandomTimeSpanFactory))]
     public sealed partial class RandomTimeSpanFactory
         : RandomScalarFactory<TimeSpan>,
