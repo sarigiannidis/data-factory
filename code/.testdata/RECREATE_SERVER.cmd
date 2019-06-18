619ab00f003442e178cc7198dcc754ce34541465
Michalis Sarigiannidis <3284819+sarigiannidis@users.noreply.github.com>
2019-06-18 15:37:38 +0200

    .

--- code/.testdata/RECREATE_SERVER.cmd
+++ code/.testdata/RECREATE_SERVER.cmd
@@ -8,15 +8,17 @@ SET DB=DF_TEST_DB
 SET MDF=%ROOT%\%DB%.mdf
 SET LDF=%ROOT%\%DB%.ldf
 
-ECHO Recreating %SERVER%...
+ECHO Cleaning up ...
 sqllocaldb stop %SERVER%    > NUL
 sqllocaldb delete %SERVER%  > NUL
+del %MDF% > NUL
+del %LDF% > NUL
+
+ECHO Recreating %SERVER% ...
 sqllocaldb create %SERVER%  > NUL
 sqllocaldb start %SERVER%   > NUL
 
-ECHO Recreating DB...
-del %MDF% > NUL
-del %LDF% > NUL
+ECHO Recreating DB ...
 sqlcmd -v DB = "%DB%" -v MDF = "%MDF%" -v LDF = "%LDF%" -S (localdb)\%SERVER% -i "%ROOT%\CREATE_DF_TEST_DB.sql"
 
 ECHO Done.
